b7dcf5cac13a881516efa008b22a99f165f0c526
Leo Bueno <leonardo@buenoinfo.net>
2024-12-16 23:28:28 -0300

    ✨ Adiciona GraphQL e melhora o status da API
    Implementa o módulo GraphQL e expande o resolver de status para incluir informações do banco de dados Oracle.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DatabaseModule } from './config/database.module';
 import { User } from './entities/user.entity';
+import { StatusResolver } from './graphql/schema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
     DatabaseModule,
     TypeOrmModule.forFeature([User])
   ],
+  providers: [StatusResolver]
 })
 export class AppModule {}
--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,17 +1,41 @@
 import { Field, ObjectType, Query, Resolver } from '@nestjs/graphql';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @ObjectType()
 export class ApiStatus {
   @Field()
   timestamp: string;
+
+  @Field()
+  databaseType: string;
+
+  @Field()
+  databaseVersion: string;
 }
 
 @Resolver()
 export class StatusResolver {
+  constructor(
+    @InjectDataSource()
+    dataSource: DataSource,
+  ) {
+    this.dataSource = dataSource;
+  }
+
+  private dataSource: DataSource;
+  
   @Query(() => ApiStatus)
-  getApiStatus() {
+  async getApiStatus() {
+    const dbQuery = await this.dataSource.query('SELECT * FROM V$VERSION');
+    const versionInfo = dbQuery[0];
+    const versionMatch = versionInfo.BANNER.match(/Oracle Database (\S+)/);
+    const version = versionMatch ? versionMatch[1] : 'Unknown';
+
     return {
       timestamp: new Date().toISOString(),
+      databaseType: 'Oracle',
+      databaseVersion: version,
     };
   }
 }
