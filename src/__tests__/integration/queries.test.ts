74bd215154172d1e9bd0dc8ffdc8f1b30a2abd36
Leo Bueno <leonardo@buenoinfo.net>
2024-12-19 00:23:54 -0300

    ♻️ Refatora testes e dependências
    - Move 'dotenv' para devDependencies
    - Adiciona configuração do dotenv nos testes de integração
    - Implementa novo teste para verificar o tipo correto de banco de dados

--- src/__tests__/integration/queries.test.ts
+++ src/__tests__/integration/queries.test.ts
@@ -1,9 +1,13 @@
+import * as dotenv from 'dotenv';
 import * as request from 'supertest';
 
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppModule } from '../../app.module';
 import { INestApplication } from '@nestjs/common';
+
+// Carrega as variáveis de ambiente do .env
+dotenv.config();
 
 describe('Queries de integração', () => {
   let app: INestApplication;
@@ -45,5 +49,26 @@ describe('Queries de integração', () => {
     expect(response.body.data.getApiStatus).toHaveProperty('dbStatus');
     expect(typeof response.body.data.getApiStatus.timestamp).toBe('string');
     expect(response.body.data.getApiStatus.dbStatus).toBeDefined();
+  });
+
+  it('deve retornar dados da query getApiStatus com o tipo correto de banco de dados', async () => {
+    const query = `
+      query {
+        getApiStatus {
+          timestamp
+          dbStatus {
+            type
+          }
+        }
+      }
+    `;
+
+    const response = await request(app.getHttpServer())
+      .post('/graphql')
+      .send({ query })
+      .expect(200);
+
+    const expectedDbType = process.env.DATABASE_TYPE || 'PostgreSQL';
+    expect(response.body.data.getApiStatus.dbStatus.type).toBe(expectedDbType);
   });
 });
